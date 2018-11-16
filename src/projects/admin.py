9608e13ba1f6edbb25a9854045e9f2aa448323bf
Jean O. Rodrigues <jeanmaskpl@gmail.com>
2018-11-16 04:24:42 -0200

    create admin screen for projects

--- src/projects/admin.py
+++ src/projects/admin.py
@@ -3,6 +3,12 @@ from django.contrib import admin
 from .models import Project, ProjectImage
 
 
+class ProjectImageInline(admin.TabularInline):
+    model = ProjectImage
+
+
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
-    pass
+    inlines = [
+        ProjectImageInline,
+    ]
